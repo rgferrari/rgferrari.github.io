709b03f92775c39779f38c210c4ce541d519e9e1
René <rgferrari@inf.com.br>
2019-01-20 22:24:26 -0200

    push 7

--- js/script.js
+++ js/script.js
@@ -1,37 +1,38 @@
+$(document).ready(function(){
 
-//change navbar background
-$(function() {
-    var element = document.getElementById("navbar");
-    $(window).on("scroll", function() {
-      if($(window).scrollTop() > 50) {
-        element.classList.add("bg-dark");
-      } else {
-        element.classList.remove("bg-dark");
-      }
+    //change navbar background
+    $(function(){
+        var element = document.getElementById("navbar");
+        $(window).on("scroll", function() {
+          if($(window).scrollTop() > 50) {
+            element.classList.add("bg-dark");
+          } else {
+            element.classList.remove("bg-dark");
+          }
+        });
     });
-});
 
-//move skillbars
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},4000);
-	});
-});
+    //move skillbars
+    $(window).scroll(function(){
+        $('.skillbar').each(function(){
+            if(isScrolledIntoView($(this))){
+                $(this).find('.skillbar-bar').animate({
+                    width:$(this).attr('data-percent')
+                },4000);
+            }
+        });
+    });
 
-//move loading bars
-$(document).ready(function() {
+    //move loading bars
     var b1 = document.querySelector(".ldBar");
     var b = new ldBar(b1);
     console.log(b);
     setInterval(function() {
         b.set(40);
     }, 1000);   
-});
+     
 
-//smooth-scroll
-$(document).ready(function(){
+    //smooth-scroll
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
 
@@ -46,7 +47,7 @@ $(document).ready(function(){
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
-            scrollTop: $(hash).offset().top - 60
+            scrollTop: $(hash).offset().top - 56
             }, 600, 'swing', function(){
         
             // Add hash (#) to URL when done scrolling (default click behavior)
@@ -54,25 +55,35 @@ $(document).ready(function(){
             });
         } // End if
     });
-});
 
-//Highlight nav-bar
-$(document).ready(function () {
-    $(document).on("scroll", onScroll);  
-});
+    //Highlight nav-bar
+    $(document).on("scroll", function(event){  
+        var scrollPos = $(document).scrollTop();
+        $('#mainNav a').each(function () {
+            var currLink = $(this);
+            var refElement = $(currLink.attr("href"));
+            if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+                $('#mainNav li').removeClass("active");
+                currLink.addClass("active");
+            }
+            else{
+                currLink.removeClass("active");
+            }
+        });
+    });  
 
-function onScroll(event){
+    function isScrolledIntoView(elem){
+        var $elem = $(elem);
+        var $window = $(window);
     
-    var scrollPos = $(document).scrollTop();
-    $('#mainNav a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#mainNav li').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-}
+        var docViewTop = $window.scrollTop();
+        var docViewBottom = docViewTop + $window.height();
+    
+        var elemTop = $elem.offset().top;
+        var elemBottom = elemTop + $elem.height();
+    
+        return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+    }
+});
+
+
